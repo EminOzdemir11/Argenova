0d9b6d29f88fd08c7d4d39dc272e3a0c309f836e
EminOzdemir11 <eminozdemir301@gmail.com>
2021-06-26 00:22:38 +0300

    Day8

--- Script/script.js
+++ Script/script.js
@@ -6,7 +6,7 @@ const phone = document.getElementById('phone');
 const button = document.getElementById('button');
 
 
-form1.addEventListener('submit', function(e){
+form1.addEventListener('submit', function(e){  
     e.preventDefault();
     checkInputs();
 });
@@ -18,24 +18,21 @@ const checkInputs = () => {
     const passwordValue = password.value.trim();
     const phoneValue = phone.value.trim();   
 
+    var regexAlphaNum = /^[a-zA-Z]+$/;
+    var regexPhone = /^[2-9]\d{2}[2-9]\d{2}\d{4}$/;
+    var regexEmail = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
 
     if (nameValue === '') {
         setErrorFor(name,'İsim boş bırakılamaz')
-        console.log('başarısız')
-    }else{
-        setSuccessFor(name)
-        console.log('başarılı');
-        
+        console.log('başarısız');
     }
+    
 
     if (emailValue === '') {
         setErrorFor(email,'Email boş bırakılamaz')
         console.log('başarısız')
     }
-    else{
-        setSuccessFor(email)
-        console.log('başarılı');
-    }
+    
 
     if (passwordValue === '') {
         setErrorFor(password,'Şifre boş bırakılamaz')
@@ -44,21 +41,51 @@ const checkInputs = () => {
     else{
         setSuccessFor(password)
         console.log('başarılı');
-
     }
 
     if (phoneValue === '') {
         setErrorFor(phone,'Telefon boş bırakılamaz')
         console.log('başarısız')
     }
-    else{
-        setSuccessFor(phone)
-        console.log('başarılı');
+    
+
+    if (nameValue !== '' && regexAlphaNum.test(nameValue) === false) {
+        setErrorFor(name,'Geçersiz isim');
+        Swal.fire({
+            icon: 'error',
+            title: 'Hoop Nereye...',
+            text: 'İsim sadece harflerden oluşmalıdır!'
+        })    
+    }else if (nameValue !== '' && regexAlphaNum.test(nameValue) === true){
+        setSuccessFor(name);
     }
-    if (nameValue !== '' && emailValue !== '' && passwordValue !== '' && phoneValue !== '') {
+
+    if (emailValue !== '' && regexEmail.test(emailValue) === false) {
+        setErrorFor(email,'Geçersiz email')
+        Swal.fire({
+            icon: 'error',
+            title: 'Hoop Nereye...',
+            text: 'Email yanlış!'
+        })
+    }else if (emailValue !== '' && regexEmail.test(emailValue) === true){
+        setSuccessFor(email);
+    }
+
+    else if (phoneValue !== '' && regexPhone.test(phoneValue) === false) {
+        setErrorFor(phone,'Geçersiz Telefon')
+        Swal.fire({
+            icon: 'error',
+            title: 'Hoop Nereye...',
+            text: 'Telefon numarası yanlış!'
+        })
+    }else if (phoneValue !== '' && regexPhone.test(phoneValue) === false){
+        setSuccessFor(phone);
+    }
+
+    if (nameValue !== '' && emailValue !== '' && passwordValue !== '' && phoneValue !== '' && regexAlphaNum.test(nameValue) === true && regexEmail.test(emailValue) === true && regexPhone.test(phoneValue) === true) {
         Swal.fire(
             'Başarılı',
-            'kayıt başarıyla tamamlandı',
+            'Kayıt başarıyla tamamlandı',
             'success'
         )  
         console.log('swal fire is success');
@@ -79,7 +106,7 @@ function setSuccessFor(input){
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
-       
+  
 
 
 
